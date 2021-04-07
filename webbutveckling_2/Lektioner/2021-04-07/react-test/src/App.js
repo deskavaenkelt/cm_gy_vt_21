f93df4fee6d7e68ec11a635e49cd26a115f7b9c6
Lars Strömberg <stromberg.lars@gmail.com>
2021-04-07 11:01:22 +0200

    create-react-app

---
+++ webbutveckling_2/Lektioner/2021-04-07/react-test/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Hej</h1>
+            <p>brödtext</p>
+            <p>
+                någonting
+            </p>
+        </>
+    );
+}
+
+export default App;
