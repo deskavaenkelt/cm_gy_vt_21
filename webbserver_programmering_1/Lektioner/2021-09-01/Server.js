739d941788ef37993118fab0ce052a37e7e6d5a8
Lars Strömberg <stromberg.lars@gmail.com>
2021-09-01 13:56:21 +0200

    Lagt till kalkylator funktionalitet

--- webbserver_programmering_1/Lektioner/2021-09-01/Server.js
+++ webbserver_programmering_1/Lektioner/2021-09-01/Server.js
@@ -20,6 +20,49 @@ app.post('/', (req, res) => {
     res.send('Got an POST request')
 })
 
+app.post('/name/:input', (req, res) => {
+    let input = req.params.input
+    res.send(`Your name is: ${input}`)
+})
+
+app.post('/addition/:num1/:num2', (req, res) => {
+    // Tolkas som strängar
+    // let number1 = req.params.num1
+    // let number2 = req.params.num2
+
+    // Tolka som nummer istället
+    let number1 = Number(req.params.num1)
+    let number2 = Number(req.params.num2)
+    let sum = number1 + number2
+    res.send(`Summan av talen ${number1} + ${number2} = ${sum}`)
+})
+
+app.post('/subtraktion/:num1/:num2', (req, res) => {
+    let number1 = Number(req.params.num1)
+    let number2 = Number(req.params.num2)
+    let sum = number1 - number2
+    res.send(`Subtraction av talen ${number1} - ${number2} = ${sum}`)
+})
+
+app.post('/multiplikation/:num1/:num2', (req, res) => {
+    let number1 = Number(req.params.num1)
+    let number2 = Number(req.params.num2)
+    let sum = number1 * number2
+    res.send(`Multiplikation av talen ${number1} * ${number2} = ${sum}`)
+})
+
+app.post('/division/:num1/:num2', (req, res) => {
+    let number1 = Number(req.params.num1)
+    let number2 = Number(req.params.num2)
+
+    if (number2 === 0) {
+        res.send(`Division av 0 är inte tillåtet`);
+    } else {
+        let sum = number1 / number2;
+        res.send(`Division av talen ${number1} / ${number2} = ${sum}`)
+    }
+})
+
 // PUT = Ändra på något som redan finns
 app.put('/', (req, res) => {
     res.send('Got an PUT request')
