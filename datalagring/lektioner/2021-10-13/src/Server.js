8f183afd8ac74556b39b03cac7a897e54cca96f6
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-13 13:53:37 +0200

    separerat middlewares

--- datalagring/lektioner/2021-10-13/src/Server.js
+++ datalagring/lektioner/2021-10-13/src/Server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import Logger from "./utils/Logger.js";
+import helmet from "helmet";
+// import morgan from "morgan";
+import MorganMiddleware from "./middlewares/MorganMiddleware.js";
+import Middlewares from "./middlewares/Middlewares.js"
 
 dotenv.config()
 const app = express()
@@ -12,17 +16,28 @@ const dbName = process.env.MONGODB_DB_NAME
 const dbCollection = process.env.MONGODB_COLLECTION
 
 // Middleware
+app.use(helmet())
+// app.use(morgan('combined'))
+app.use(MorganMiddleware)
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(Middlewares.errorHandler)
+
+app.get('/users', (req, res) => {
+    res.send('Users api url')
+})
 
 // Api alive route
 app.get('/', (req, res) => {
     res.send('Api is Alive!')
 })
+
+// Place After all valid urls
+app.use(Middlewares.notFound)
 
 function isServerInDevelopmentMode() {
     const devEnv = 'development'
