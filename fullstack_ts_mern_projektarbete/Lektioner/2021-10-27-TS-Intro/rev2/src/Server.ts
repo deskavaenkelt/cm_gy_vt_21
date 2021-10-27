8592a2fd72a699b849d6c0fa8f244c37e4efe7b7
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-27 12:02:28 +0200

    Backend i TS r2 med nodemon

---
+++ fullstack_ts_mern_projektarbete/Lektioner/2021-10-27-TS-Intro/rev2/src/Server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+    res.send('API is Alive with TypeScript! and Nodemon')
+})
+
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server started at http://localhost:${ port }`)
+})
