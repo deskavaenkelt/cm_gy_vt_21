ededb72ed7e204feb08ecd10bae56d753bff1bdd
Lars Strömberg <stromberg.lars@gmail.com>
2021-12-01 12:02:53 +0100

    fungerande counter med ökning och minskning

--- fullstack_ts_mern_projektarbete/Lektioner/2021-12-01-ReactRouterBcrypt/frontend/src/view/CounterView.tsx
+++ fullstack_ts_mern_projektarbete/Lektioner/2021-12-01-ReactRouterBcrypt/frontend/src/view/CounterView.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { PrimaryButton, SecondaryButton } from '../components/CustomButtonComponent'
 
 const CounterView = () => {
+	const [count, setCount] = useState<number>(0)
+	
+	useEffect(() => {
+		console.log('Component is being rendered in DOM')
+		
+		return () => {
+			console.log('--== Component is being removed from DOM ==--')
+		}
+	}, [count])
+	
 	return (
 		<>
 			<h1>CounterView</h1>
+			<h1>{ count }</h1>
+			<PrimaryButton onClick={ () => setCount(count + 1) } children={ 'Increment by 1' }/>
+			<SecondaryButton onClick={ () => setCount(count - 1) } children={ 'Decrement by 1' }/>
 		</>
 	)
 }
